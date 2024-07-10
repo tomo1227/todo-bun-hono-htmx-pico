2fe081b0488b5881b6833f95322387ac4fcc43a9
tomo1227 <tomo.soccer1227@gmail.com>
2024-07-10 19:20:05 +0000

    npmからcurlに変更

--- .devcontainer/scripts/post_create.sh
+++ .devcontainer/scripts/post_create.sh
@@ -1,7 +1,7 @@
-#!/usr/bin/env sh
+# !/usr/bin/bash
+wget -qO- https://bun.sh/install | bash
+wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -O ~/.git-completion.bash
+echo "source ~/.git-completion.bash" >> ~/.bashrc
 
-# wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -O ~/.git-completion.bash
-# echo ". ~/.git-completion.bash" >> ~/.bashrc
-
-# # bash auto completion
+# bash auto completion
 # sudo apt update && sudo apt install bash-completion
--- .devcontainer/scripts/post_start.sh
+++ .devcontainer/scripts/post_start.sh
@@ -1,15 +1,16 @@
+#!/usr/bin/bash
+
 if [ -z "$1" ]; then
-  echo "Error: No directory provided."
-  exit 1
+    echo "Error: No directory provided."
+    exit 1
 fi
 
-# #  ~/.gitconfigをローカルからコピーする
+#  ~/.gitconfigをローカルからコピーする
 # echo "start to copy local ~/.gitconfig"
 # git config --global --add safe.directory $1
 
-# # ~/.gitconfigの設定追加
+# ~/.gitconfigの設定追加
 # git config pull.rebase false
 # git config --global commit.template .commit_template
 
-npm install -g bun
-bun i
+bun install
